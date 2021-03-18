c3570bcd6661d6734d0057ae1e308b38af559391
dhruvdadhania <dhruvdadhania@gmail.com>
2021-03-18 15:07:28 -0400

    Train games from GUI

--- game_instantiator.py
+++ game_instantiator.py
@@ -4,15 +4,15 @@ from gym.envs.atari.atari_env import AtariEnv
 
 
 class GameInstantiator():
-    def run_cartpole(self):
-        return run_gym_game("CartPole-v1", CartPoleEnv())
+    def run_cartpole(self, n_iter):
+        return run_gym_game("CartPole-v1", CartPoleEnv(), n_iter)
 
-    def run_space_invaders(self):
-        return run_gym_game("SpaceInvaders-v0", AtariEnv())
+    def run_space_invaders(self, n_iter):
+        return run_gym_game("SpaceInvaders-v0", AtariEnv(), n_iter)
 
-    def run_mountain_car(self):
-        return run_gym_game("MountainCar-v0", MountainCarEnv())
+    def run_mountain_car(self, n_iter):
+        return run_gym_game("MountainCar-v0", MountainCarEnv(), n_iter)
 if __name__ == "__main__":
     g = GameInstantiator()
     # g.run_space_invaders()
-    g.run_mountain_car()
+    # g.run_mountain_car()
