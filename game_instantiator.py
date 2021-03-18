24074de0802f4a657a0bb6ee030e20903d1fea40
Kunal Upadya <kunal.upadya@gmail.com>
2021-03-18 15:09:13 -0400

    save before merge

--- game_instantiator.py
+++ game_instantiator.py
@@ -1,6 +1,6 @@
 from openaigymexample import run_gym_game
 from gym.envs.classic_control import CartPoleEnv, MountainCarEnv
-from gym.envs.atari.atari_env import AtariEnv
+from gym.envs.atari import AtariEnv
 
 
 class GameInstantiator():
@@ -14,5 +14,5 @@ class GameInstantiator():
         return run_gym_game("MountainCar-v0", MountainCarEnv())
 if __name__ == "__main__":
     g = GameInstantiator()
-    # g.run_space_invaders()
-    g.run_mountain_car()
+    g.run_space_invaders()
+    # g.run_mountain_car()
