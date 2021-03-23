2d1798642f839bddd980d769f0a60bbff0f102ec
dhruvdadhania <dhruvdadhania@gmail.com>
2021-03-23 14:55:11 -0400

    Train games from GUI

--- game_instantiator.py
+++ game_instantiator.py
@@ -1,21 +1,24 @@
-from openaigymexample import run_gym_game
+from openaigymexample import run_gym_game, init_gym_game
 from gym.envs.classic_control import CartPoleEnv, MountainCarEnv
 from gym.envs.atari import AtariEnv
 
 
 class GameInstantiator():
+    def __init__(self, testing = False):
+        self.game_call = run_gym_game if testing else init_gym_game
+
     def run_cartpole(self, n_iter = 5):
-        return run_gym_game("CartPole-v1", CartPoleEnv(), n_iter)
+        return self.game_call("CartPole-v1", CartPoleEnv(), n_iter)
 
     def run_pong(self, n_iter = 5):
-        return run_gym_game("Pong-v0", AtariEnv(game="pong"), n_iter)
+        return self.game_call("Pong-v0", AtariEnv(game="pong"), n_iter)
 
     def run_space_invaders(self, n_iter = 5):
-        return run_gym_game("SpaceInvaders-v0", AtariEnv(game="space_invaders"), n_iter)
+        return self.game_call("SpaceInvaders-v0", AtariEnv(game="space_invaders"), n_iter)
 
     def run_mountain_car(self, n_iter = 5):
-        return run_gym_game("MountainCar-v0", MountainCarEnv(), n_iter)
+        return self.game_call("MountainCar-v0", MountainCarEnv(), n_iter)
 if __name__ == "__main__":
-    g = GameInstantiator()
+    g = GameInstantiator(True)
     g.run_space_invaders(5)
     # g.run_mountain_car()
